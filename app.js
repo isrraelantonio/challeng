ea12781e836c6df91a4a6efd9e627e66b0648a32
Israel Antonio <israelantonio027@gmail.com>
2025-01-22 13:01:36 -0300

    Criação da lista amigos e criação daas funções adicionar amigo e limpar campo.

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//declaração das varáveis a serem utilizadas no código.
+
+let amigos = []
+
+// Criação da função adicionarAmigo. A mesma coleta o valor do input, verifincando inclusive se o mesmo é vázio ou não para tratamento de excessões. 
+
+function adcionoarAmigo(){
+
+    let amigo =  document.querySelector('input').value
+
+
+        if(amigo == ''){ // input vazio?
+              alert("digite um nome")
+        }else{
+            
+            if(amigos.includes(amigo)){ // valor cnsta na lista ?
+                alert("O seu amigo(a) já está na lista")
+                limparCampo()
+            }else{ // faça isso então
+              amigos.push(amigo)
+              apresentar()
+              limparCampo()
+              
+            } 
+           
+        }
+}
+
+function limparCampo(){ // função para limpar o input
+    let limpar = document.querySelector('input');
+    limpar.value = '';
+  }
