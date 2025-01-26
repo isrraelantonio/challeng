eb7b64a92ace7c2a10bdb061ccfe279120bf1701
Israel Antonio <israelantonio027@gmail.com>
2025-01-26 11:00:46 -0300

    retirada da variável do escopo global e adiconada a função principal. uso do método trim() para limpar os espaçõs do imput, melhhoria na mensagem do sorteio

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //declaração das varáveis a serem utilizadas no código.
 
-let amigos = []
+
 
 // Criação da função adicionarAmigo. A mesma coleta o valor do input, verifincando inclusive se o mesmo é vázio ou não para tratamento de exceções. 
 
 function adicionarAmigo(){
-
-    let amigo =  document.querySelector('input').value
+    let amigos = []
+    let amigo =  document.querySelector('input').value.trim();
 
 
         if(amigo == ''){ // input vazio?
@@ -53,14 +53,12 @@ function apresentar(){
 function sortearAmigo(){
 
     let div = document.getElementById('resultado');
-    let lista = document.getElementById('lista');
-    lista.innerHTML = '';
     
       if (amigos.length == 0){
         alert("adicione amigos a sua lista")
       }else{ 
         amigoPremiado = amigos[parseInt(Math.random() * amigos.length)]
-        div.innerHTML = `${amigoPremiado} `;
+        div.innerHTML =`<strong>${amigoPremiado}</strong> foi sorteado!`;
       }
   }
   
